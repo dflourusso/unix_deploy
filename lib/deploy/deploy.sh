8c3f0cbe2f902c8d06d6b665134167f38252efd7
Daniel Fernando Lourusso <dflourusso@gmail.com>
2016-06-22 19:24:47 -0300

    without heroku

--- lib/deploy/deploy.sh
+++ lib/deploy/deploy.sh
@@ -68,7 +68,7 @@ else
   cd $APP_NAME
 
   echo $'\e[34mInstalando dependencias da aplicacao...\e[0m'
-  /home/$USER/.rbenv/shims/bundle install --deployment --without development test
+  /home/$USER/.rbenv/shims/bundle install --deployment --without heroku development test
 
 
   # Configuracao do NGINX
